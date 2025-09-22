8a3eccdf8444a130fe2b3723efd9f00b2638e10f
Richard <richard.poruthoor3506@gmail.com>
2025-09-22 12:25:21 -0500

    function works, added user input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
-
-
 #include "src/hello.hpp"
+#include <string>
+
+using namespace std; 
+
+// compares 2 strings case insensitive
+int strcmp_case_insensitive(string str1, string str2) {
+
+  // finds smaller str
+  string smallstr;
+  if (str1.length() <= str2.length()) {
+    smallstr = str1;
+  } else {
+    smallstr = str2;
+  }
+
+  // compares each char
+  for (size_t i = 0; i < smallstr.size(); i++) {
+    char c1 = tolower(str1[i]);
+    char c2 = tolower(str2[i]);
+
+    if (c1 > c2) {
+      return 1;
+    } else if (c1 < c2) {
+      return -1;
+    }
+  }
+
+  // if they're the same till now, check length
+  if (str1.length() > str2.length()) {
+    return 1;
+  } else if (str1.length() < str2.length()) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
 
 int main() {
-  return 0;
-}
+  string first;
+  string second;
+
+  std::cout << "Enter first string to compare (case insensitive): ";
+  std::cin >> first;
+  std::cout << "Enter second string: ";
+  std::cin >> second;
+  std::cout << strcmp_case_insensitive(first, second) << "\n";
+}   
+
